65b4ec7a7910fe2ca7dba1924e85ac926ef31aea
Patricio GUZMAN <pguzmanosorio@gmail.com>
2017-10-24 15:31:36 +0200

    first commit

---
+++ swifty_protein/swifty_protein/LoginViewController.swift
@@ -0,0 +1,62 @@
+
+import UIKit
+import LocalAuthentication
+import ChameleonFramework
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var enter: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var touchId: UIButton!
+    
+    var lacontext: LAContext!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.lacontext = LAContext()
+        self.setUi()
+    }
+    
+    func setUi() {
+        
+        self.view.backgroundColor = UIColor.flatWhite
+        if lacontext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+            self.touchId.isHidden = false
+            self.passwordTextField.isHidden = true
+            self.enter.isHidden = true
+        }
+        else {
+            self.touchId.isHidden = true
+            self.passwordTextField.isHidden = false
+            self.enter.isHidden = false
+        }
+    }
+    
+    func showTouchId() {
+        if lacontext.canEvaluatePolicy(.deviceOwnerAuthentication, error: nil) {
+            lacontext.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "Please verify yourself", reply: {(isYou, error) in
+                DispatchQueue.main.async {
+                    if isYou {
+                        print("Do segue")
+                    }
+                    else {
+                        DispatchQueue.main.async {
+                            let alert = UIAlertController(title: "Error", message: "something went wrong", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                }
+            })
+        }
+        else {
+            self.touchId.isHidden = true
+        }
+    }
+    
+    @IBAction func clickFingerprint(_ sender: Any) {
+        self.showTouchId()
+    }
+    
+}
+
