6d3579637853edaa5cfd5cf5bf6177b5f168c565
Patricio GUZMAN <pguzmanosorio@gmail.com>
2017-10-24 17:55:13 +0200

    Searchbar was implemented

--- swifty_protein/swifty_protein/ProteinTableViewController.swift
+++ swifty_protein/swifty_protein/ProteinTableViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ProteinTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+class ProteinTableViewController: UIViewController  {
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableview: UITableView!
     var proteinList: [String]!
+    var filteredData: [String]?
+    var isSearching: Bool = false
     var i = 0
     var textSelected: String?
     
@@ -19,26 +22,11 @@ class ProteinTableViewController: UIViewController, UITableViewDelegate, UITable
         super.viewDidLoad()
         let session = UserDefaults.standard
         
+        tableview.delegate = self
+        tableview.dataSource = self
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.done
         proteinList = session.array(forKey: "ligands") as? [String]
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return proteinList?.count ?? 0
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "proteinCell") as? ProteinTableViewCell {
-            cell.name.text = self.proteinList?[indexPath.row] ?? "default"
-            return cell
-        }
-        return UITableViewCell()
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("i selected a row")
-        self.textSelected = self.proteinList[indexPath.row]
-        self.performSegue(withIdentifier: "TableToProtein", sender: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -49,6 +37,59 @@ class ProteinTableViewController: UIViewController, UITableViewDelegate, UITable
                     vc.proteinToDisplay = nbTitle
                 }
             }
+        }
+    }
+}
+
+extension ProteinTableViewController:  UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.isSearching {
+            return filteredData?.count ?? 0
+        }
+        return proteinList?.count ?? 0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "proteinCell") as? ProteinTableViewCell {
+            if isSearching {
+                cell.name.text = self.filteredData?[indexPath.row] ?? "default"
+            }
+            else {
+                cell.name.text = self.proteinList?[indexPath.row] ?? "default"
+            }
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("i selected a row")
+        self.textSelected = self.proteinList[indexPath.row]
+        self.performSegue(withIdentifier: "TableToProtein", sender: nil)
+    }
+}
+
+extension ProteinTableViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+            self.isSearching = false
+            
+            view.endEditing(true)
+            
+            tableview.reloadData()
+        }
+        else {
+            self.isSearching = true
+            filteredData = proteinList.filter {
+                if  $0.range(of: searchText) == nil {
+                    return false
+                }
+                return true
+            }
+            tableview.reloadData()
         }
     }
 }
