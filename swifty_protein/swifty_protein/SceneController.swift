6551b19e7654d3185676ef9048eb691e379342a8
Patricio GUZMAN <pguzmanosorio@gmail.com>
2017-10-27 11:15:51 +0200

    TEXT OVER ATOMS ARE DISPLAY :D

--- swifty_protein/swifty_protein/SceneController.swift
+++ swifty_protein/swifty_protein/SceneController.swift
@@ -26,8 +26,8 @@ class SceneController {
         self.scnView.autoenablesDefaultLighting = true
         self.scnView.allowsCameraControl = true
         
-//        let tap = UITapGestureRecognizer(target: self, action: #selector(handleFrontTap(sender:)))
-//        scnView.addGestureRecognizer(tap)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleFrontTap(sender:)))
+        scnView.addGestureRecognizer(tap)
         scnView.isUserInteractionEnabled = true
     }
     
@@ -70,17 +70,37 @@ class SceneController {
         self.ligandToDisplay = ligand
     }
     
-//    @objc func handleFrontTap(sender: UITapGestureRecognizer) {
-//        if sender.state == UIGestureRecognizerState.recognized
-//        {
-//            let location: CGPoint = sender.location(in: sender.view)
-//            let hits = self.scnView.hitTest(location, options: nil)
-//            if let tappedNode = hits.first?.node {
-//                print(tappedNode)
-//                // do something with the tapped node ...
-//            }
-//        }
-//    }
+    @objc func handleFrontTap(sender: UITapGestureRecognizer) {
+        if sender.state == UIGestureRecognizerState.recognized
+        {
+            let location: CGPoint = sender.location(in: sender.view)
+            let hits = self.scnView.hitTest(location, options: nil)
+            if let tappedNode = hits.first?.node {
+                if tappedNode.childNodes.count > 0 {
+                    tappedNode.childNodes[0].removeFromParentNode()
+                }
+                else {
+                    self.setupText(tappedNode: tappedNode)
+                }
+            }
+            else { // click outside of the ligand's structure, it will remove all the text
+                for node in scnScene.rootNode.childNodes {
+                    if node.name != nil, node.childNodes.count > 0 {
+                        node.childNodes[0].removeFromParentNode()
+                    }
+                }
+            }
+        }
+    }
+    
+    func setupText(tappedNode: SCNNode){
+        let textNode = SCNNode()
+        let text = SCNText(string: tappedNode.name, extrusionDepth: 2)
+        textNode.geometry = text
+        textNode.scale = SCNVector3(0.04, 0.04, 0.04)
+        textNode.position = SCNVector3Make(0, 0, 0)
+        tappedNode.addChildNode(textNode)
+    }
     
     func displayLigand() {
         if let ligand = self.ligandToDisplay {
@@ -99,7 +119,7 @@ class SceneController {
         
         let geometryNode = SCNNode(geometry: geometry)
         geometryNode.position = SCNVector3(x: atom.x, y: atom.y, z: atom.z)
-        
+        geometryNode.name = atom.elementAndNumber
         scnScene.rootNode.addChildNode(geometryNode)
     }
     
@@ -109,11 +129,11 @@ class SceneController {
         let v1 = SCNVector3(x: (firstAtom?.x)!, y: (firstAtom?.y)!, z: (firstAtom?.z)!)
         let v2 = SCNVector3(x: (secondAtom?.x)!, y: (secondAtom?.y)!, z: (secondAtom?.z)!)
         
-//        if (firstAtom?.x == secondAtom?.x && firstAtom?.y == secondAtom?.y) {
-//            print("SAME")
-//            print(firstAtom?.z)
-//            print(secondAtom?!.z)
-//        }
+        //        if (firstAtom?.x == secondAtom?.x && firstAtom?.y == secondAtom?.y) {
+        //            print("SAME")
+        //            print(firstAtom?.z)
+        //            print(secondAtom?!.z)
+        //        }
         self.generateCylinder(atom1: v1, atom2: v2)
     }
     
@@ -163,3 +183,4 @@ private extension SCNVector3{
         }
     }
 }
+
