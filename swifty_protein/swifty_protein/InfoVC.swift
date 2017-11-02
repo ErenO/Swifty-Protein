3da59e95af8aac66bbbeeaffeb93dc81b22a7f46
Patricio GUZMAN <pguzmanosorio@gmail.com>
2017-11-02 16:51:23 +0100

    delete useless things

--- swifty_protein/swifty_protein/InfoVC.swift
+++ swifty_protein/swifty_protein/InfoVC.swift
@@ -28,7 +28,6 @@ class InfoVC: UIViewController {
     @IBOutlet weak var viewInScrollView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-//        myScrollView.contentSize = CGSize(width: view.frame.size.width, height: view.frame.size.height)
             
         nameTxt.text = name
         formulaTxt.text = formula
@@ -37,22 +36,6 @@ class InfoVC: UIViewController {
         nameTxt.lineBreakMode = .byWordWrapping // notice the 'b' instead of 'B'
         nameTxt.numberOfLines = 0
     }
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
